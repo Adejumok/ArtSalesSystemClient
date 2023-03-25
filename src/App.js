7849db28a3914c52dd06a454d7dc4df6f9bc40c5
Adejumok <roseadeyinka01@gmail.com>
2023-03-25 14:46:25 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import NavBar from './layout/NavBar'
+import ArtDB from './components/ArtsDB'
+import Home from './components/Home'
+import { Route, Routes } from 'react-router-dom';
+import AddArt from './arts/AddArt';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/addArt' element={<AddArt/>}/>
+        <Route path='/artDb' element={<ArtDB/>}/>
+      </Routes>
+    </>
   );
 }
 
